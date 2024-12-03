c4e33949791bd4d284ce4d062ca9e4362dd1a775
Ubuntu <ubuntu@ip-172-31-24-112.us-east-2.compute.internal>
2024-12-03 16:09:27 +0000

    vectorized trips + recommendation complete

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-  backendUrl: 'http://18.216.204.24:8081',  // Update this to your backend URL
+  backendUrl: 'http://3.140.253.15:8081',  // Update this to your backend URL
 };
 
 export default config;
